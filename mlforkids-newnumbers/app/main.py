4eb897e8c713fcfd2285d3be14f864644786a019
dusx1981 <157097695@qq.com>
2025-09-26 22:03:14 +0800

    更新图片地址信息

--- mlforkids-newnumbers/app/main.py
+++ mlforkids-newnumbers/app/main.py
@@ -99,12 +99,12 @@ async def model_training_request(scratch_key: str, csvfile: UploadFile,
     info("%s : Returning status", scratch_key)
     return savedmodel
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        "app.main:app", 
-        host="0.0.0.0", 
-        port=8000, 
-        reload=True,
-        log_level="debug"
-    )
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(
+#         "app.main:app", 
+#         host="0.0.0.0", 
+#         port=8000, 
+#         reload=True,
+#         log_level="debug"
+#     )
